147c9f1482e7a00ccb84d2f996abd16c7fc38a23
Vinicius Ghellere Sbardelotto <vinicius.ghellere@geekhunter.com.br>
2021-10-30 12:37:40 -0300

    feat: add next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,34 @@
 import type { AppProps } from 'next/app';
+import { NextSeo } from 'next-seo';
 import { ChakraProvider } from '@chakra-ui/react';
+
+import theme from 'lib/settings/theme';
+
+const metaTags = [
+  {
+    name: 'viewport',
+    content:
+      'width=device-width, initial-scale=1, maximum-scale=5, shrink-to-fit=no',
+  },
+  {
+    name: 'Content-Security-Policy',
+    content: 'upgrade-insecure-requests',
+  },
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <NextSeo
+        title='Universal Converter'
+        description='Universal Converter tools - calculators, tables, formulas, math. Temperature, weight, area, volume, length.'
+        canonical='https://www.universalconverter.org/'
+        additionalMetaTags={metaTags}
+      />
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
